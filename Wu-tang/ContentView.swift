4813ba1573fab5793d1184b30b899588bdabaee5
Gema <saucedogema8@gmail.com>
2026-04-17 12:30:44 -0700

    Initial Commit

---
+++ Wu-tang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wu-tang
+//
+//  Created by SAUCEDO, GEMA on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wu-tang/Wu_tangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_tangApp.swift
+//  Wu-tang
+//
+//  Created by SAUCEDO, GEMA on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Wu_tangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
